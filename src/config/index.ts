e7bdf92b0f2aa7ad6bb43ad86f4aa53cfe4e2568
twentyone37 <twentyone_37@proton.me>
2024-02-12 13:00:30 -0300

    Update cors

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,6 +9,6 @@ export const config = {
     "4GVrk3J7GB8a29RqqBG6x9WZieDufp3ngPc6zcRH3JzfQN3WxLjMbgf98feTWdivSoXxM5EqpQQtzZzrAG6qJDMs",
   mongoUri: process.env.MONGO_URI || "mongodb://localhost:27017/merchants",
   port: process.env.PORT || 3000,
-  frontendUrl: process.env.FRONTEND_URL || "https://www.cryptomapp.vercel.app",
+  frontendUrl: process.env.FRONTEND_URL || "https://cryptomapp.vercel.app",
   usdcMintAddress: process.env.CUSTOM_USDC_MINT,
 };
