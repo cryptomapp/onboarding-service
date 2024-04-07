671ee38764e5099d55695e63edf1448c7fd0a6b8
twentyone37 <twentyone_37@proton.me>
2024-04-07 19:55:13 +0200

    [temp] Add converter

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes";
 import { config } from "./config";
+import {
+  convertARStoUSDC,
+  convertUSDCToARS,
+} from "./utils/blueDollarConverter";
 
 const app = express();
 app.use(
@@ -13,5 +17,36 @@ app.use(express.json());
 
 // Use the defined routes
 app.use("/api/user", userRoutes);
+
+// TODO: DELETE IT!
+// POST endpoint for ARS to USDC conversion
+app.post("/convert-ars-to-usdc", async (req, res) => {
+  try {
+    const { amount } = req.body;
+    if (!amount) {
+      return res.status(400).send({ error: "ARS amount is required" });
+    }
+    const usdcValue = await convertARStoUSDC(amount);
+    return res.json({ arsValue: amount, usdcValue });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ error: "Error converting currency" });
+  }
+});
+
+// POST endpoint for USDC to ARS conversion
+app.post("/convert-usdc-to-ars", async (req, res) => {
+  try {
+    const { amount } = req.body;
+    if (!amount) {
+      return res.status(400).send({ error: "USDC amount is required" });
+    }
+    const arsValue = await convertUSDCToARS(amount);
+    return res.json({ usdcValue: amount, arsValue });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ error: "Error converting currency" });
+  }
+});
 
 export default app;
